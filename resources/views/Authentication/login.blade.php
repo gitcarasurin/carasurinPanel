dd87a13af148e9e832ac2845be85192406f3ddfe
azimi <joorjin2@gmail.com>
2021-11-23 10:23:37 +0330

    first commit

---
+++ resources/views/Authentication/login.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>login</title>
+
+    <!-- bootstrap rtl -->
+    <link rel="stylesheet" href="dist/css/bootstrap-rtl.min.css">
+</head>
+<body>
+    
+
+
+    <div class="row">
+        <div class="col-lg-3 col-6">
+            <form method="post">
+                
+            </form>
+        </div>
+    </div>
+
+    <!-- jQuery -->
+    <script src="plugins/jquery/jquery.min.js"></script>
+</body>
+</html>
---
+++ routes/web.php
@@ -0,0 +1,17 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+
+
+Route::get('/', function () {
+    return view('dashboard.index');
+});
+
+
+Route::view('login', '/');
+
+
+// Authentication
+
+///////////////////
