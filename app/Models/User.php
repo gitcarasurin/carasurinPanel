08b1d5d1dfe93a99472c8c6f3923ce54a97dbc50
azimi <joorjin2@gmail.com>
2021-11-24 16:40:48 +0330

    add log plugin

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,13 +7,15 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
-
+use Spatie\Activitylog\Models\Activity;
 class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
 
+
     public function legals()
     {
+        Activity::all();
         return $this->hasOne('App\Legals');
     }
 
